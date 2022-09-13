7ca093590c07ddb3e6de41e986b807e15b337125
Roger Toledo <roger.toledo@olxbr.com>
2022-09-13 07:48:49 -0300

    Add log to DeafaultApi

--- practical-java/src/main/java/me/rto/practicaljava/api/server/DefaultRestApi.java
+++ practical-java/src/main/java/me/rto/practicaljava/api/server/DefaultRestApi.java
@@ -1,14 +1,26 @@
 package me.rto.practicaljava.api.server;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
 
 @RestController
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
 
+    private Logger logger = LoggerFactory.getLogger(DefaultRestApi.class);
     @RequestMapping(value = "/welcome")
     public String welcome(){
+        logger.info(StringUtils.join("Hello", " this is", "Spring boot", " REST API"));
         return "Welcome to Spring Boot";
+    }
+
+    @RequestMapping(value = "/time")
+    public String time() {
+        return LocalTime.now().toString();
     }
 }
