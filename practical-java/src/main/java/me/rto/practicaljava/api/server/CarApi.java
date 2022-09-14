64d6bf45089ab98579cd3fc5929798ce7d46ec2a
Roger Toledo <roger.toledo@olxbr.com>
2022-09-14 08:22:03 -0300

    feat: Class 31

--- practical-java/src/main/java/me/rto/practicaljava/api/server/CarApi.java
+++ practical-java/src/main/java/me/rto/practicaljava/api/server/CarApi.java
@@ -2,11 +2,11 @@ package me.rto.practicaljava.api.server;
 
 import me.rto.practicaljava.entity.Car;
 import me.rto.practicaljava.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 
@@ -14,11 +14,20 @@ import java.awt.*;
 @RestController
 public class CarApi {
 
+    private static final Logger logger = LoggerFactory.getLogger(CarApi.class);
+
     @Autowired
     private CarService carService;
 
     @GetMapping(value = "/random", produces = MediaType.APPLICATION_JSON_VALUE)
     public Car random() {
         return carService.generateCar();
+    }
+
+    @PostMapping(value = "/echo", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String echo(@RequestBody Car car){
+        logger.info("Car is {}", car);
+
+        return car.toString();
     }
 }
